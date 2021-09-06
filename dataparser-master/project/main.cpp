92ae7eecdd841386826ec596ca4a48427c72420e
chibiandetianye <xinhong007@126.com>
2021-09-06 14:29:49 +0800

    update

--- dataparser-master/project/main.cpp
+++ dataparser-master/project/main.cpp
@@ -1,11 +1,14 @@
 // #define CHECK_TIME
 #include"szData_parser.h"
 
+#include<stdlib.h>
+
 #include<iostream>
 
 using namespace parser;
 
 int main(){
+    system("mkdir deal;mkdir order");
     szDataParser parser{};
     parser.main_run("20210709_91401215.szdata");
     return 0;
