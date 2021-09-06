eba845b421c4cbdf489137b79517aeb00bbab0ad
chibiandetianye <xinhong007@126.com>
2021-09-06 10:34:40 +0800

    update

--- inc/common.h
+++
@@ -1,9 +0,0 @@
-#ifndef _COMMON_INCLUDE_H_
-#define _COMMON_INCLUDE_H_
-
-# define likely(x)  __builtin_expect(!!(x), 1)
-# define unlikely(x)    __builtin_expect(!!(x), 0)
-
-#define packed __attribute__((packed))
-
-#endif /**_COMMON_INCLUDE_H_*/
